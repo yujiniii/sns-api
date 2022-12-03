8ba83ba2b1a68e3a04bac5c775e4bcb54c28719f
유진 <yujiniii@icloud.com>
2022-12-04 00:49:14 +0900

    안되는 부분 주석

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MySQLDataSource } from 'src/config/typeorm.config';
+// import { MySQLDataSource } from 'src/config/typeorm.config';
 import { User } from 'src/users/entities/user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { Board } from './entities/board.entity';
 
-const connection = MySQLDataSource2.initialize();
+// const connection = MySQLDataSource.initialize();
 
 @Injectable()
 export class BoardsService {
@@ -39,30 +39,6 @@ export class BoardsService {
   }
 
   async findOne(id: number) {
-    // ----------- 조회수 +1 트랜잭션 -------------
-    const qRunner = await MySQLDataSource.createQueryRunner();
-    await qRunner.connect();
-    await qRunner.startTransaction();
-    try {
-      console.log('startTransaction');
-      await qRunner.manager
-        .createQueryBuilder()
-        .update()
-        .where('boardId = :id', { id: id })
-        .set({ watchCount: () => 'watchCount+1' })
-        .execute();
-      await qRunner.commitTransaction();
-      console.log('commitTransaction');
-    } catch (err) {
-      console.log('rollbackTransaction');
-      console.log(err);
-      await qRunner.rollbackTransaction();
-    } finally {
-      console.log('release');
-      await qRunner.release();
-    }
-    // -----------------------------------------
-    const theBoard = await this.boardRepo.findBy({ boardId: id }); // 조회수 변경 이후  조회
     return theBoard;
   }
 
@@ -96,5 +72,57 @@ export class BoardsService {
       }
     }
     return hashtags_db;
+  }
+  // async transaction(data: DataSource, boardId: number) {
+  //   // ----------- 조회수 +1 트랜잭션 -------------(안됨 힝구리퐁퐁)
+  //   const qRunner = await data.createQueryRunner();
+  //   await qRunner.connect();
+  //   await qRunner.startTransaction();
+  //   try {
+  //     console.log('startTransaction');
+  //     await qRunner.manager
+  //       .createQueryBuilder()
+  //       .update()
+  //       .set({ watchCount: 'watchCount+1' })
+  //       .where('boardId = :boardId', { boardId: boardId })
+  //       .execute();
+  //     await qRunner.commitTransaction();
+  //     console.log('commitTransaction');
+  //   } catch (err) {
+  //     console.log('rollbackTransaction');
+  //     console.log(err);
+  //     await qRunner.rollbackTransaction();
+  //   } finally {
+  //     console.log('release');
+  //     await qRunner.release();
+  //   }
+  // }
+  async foundEmail(email: string) {
+    const user = await this.userRepo
+      .findOne({
+        where: { email: email },
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new NotFoundException('사용자가 없습니다.');
+      });
+    if (!user || typeof user === 'undefined') {
+      throw new NotFoundException(`사용자(${email})가 없습니다.`);
+    } else {
+      return user;
+    }
+  }
+  async foundBoard(id: number) {
+    const board = await this.boardRepo
+      .findOne({ where: { boardId: id } })
+      .catch((err) => {
+        console.log(err);
+        throw new NotFoundException('게시글이 없습니다.');
+      });
+    if (!board || typeof board === 'undefined') {
+      throw new NotFoundException(`게시글(no.${id})이 없습니다.`);
+    } else {
+      return board;
+    }
   }
 }
