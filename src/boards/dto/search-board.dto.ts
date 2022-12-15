b871607b1ac63d1f43243190ac66493b98a6f0f7
유진 <yujiniii@icloud.com>
2022-12-16 01:40:29 +0900

    fix: validator 수정
     @IsNumberString() 을 해두면 쿼리에서 값을 주지 않았을 때에도 검사하기 때문에 삭제함

--- src/boards/dto/search-board.dto.ts
+++ src/boards/dto/search-board.dto.ts
@@ -1,5 +1,4 @@
-// type Ordering = 'createdAt' | 'likeCount' | 'watchCount';
-import { IsEnum, IsNumberString } from 'class-validator';
+import { IsEnum } from 'class-validator';
 
 enum SORTING {
   ASC = 'ASC',
@@ -19,8 +18,6 @@ export class SearchBoardDto {
   orderBy = ORDERING.watchCount; // 작성일(writeDate), 좋아요 수(likeCount), 조회수(watchCount) 기준
   @IsEnum(SORTING)
   isAsc = SORTING.ASC; // 오름차순
-  @IsNumberString()
   page: string; // 페이지당 게시글 개수
-  @IsNumberString()
   pageNo: string; // 페이지 정보
 }
