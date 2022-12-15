ba6b83882a7a6dc996fe23a0fb7cc05ef467f89f
유진 <yujiniii@icloud.com>
2022-12-15 23:52:24 +0900

    refactor: DTO 에 class-validator 적용

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,5 +1,13 @@
+import { Contains, IsNotEmpty } from 'class-validator';
+
 export class CreateBoardDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   content: string;
+
+  @IsNotEmpty()
+  @Contains('#')
   hashtags: string; // 나중에 파싱
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+  @IsNotEmpty()
   nickname: string;
+  @IsNotEmpty()
   password: string;
 }
