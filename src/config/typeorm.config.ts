e6e0cca17529d5b15cd8a13e8457c6611af585b3
유진 <yujiniii@icloud.com>
2022-12-16 01:39:18 +0900

    refactor: db log 안보이게하기

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,6 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
-import { DataSource } from 'typeorm';
 
 dotenv.config();
 
@@ -11,16 +10,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  logging: true,
+  logging: false,
   entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
-
-// export const MySQLDataSource = new DataSource({
-//   type: 'mysql',
-//   host: process.env.DATABASE_HOST,
-//   port: 3306,
-//   username: process.env.DATABASE_USERNAME,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE_NAME,
-// });
