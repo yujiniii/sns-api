39cb65cdecd1decaf1af86b03f0b3305734c102c
유진 <yujiniii@icloud.com>
2022-12-22 00:39:59 +0900

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,9 +4,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DATABASE_HOST,
-  port: 3306,
+  port: 5432,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
